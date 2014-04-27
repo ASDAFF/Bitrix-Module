a6b0eda47f6e79601ceed58ff148c7bda8941dc6
Svel <Svel.Sontz@gmail.com>
2014-04-27 16:30:14 +0400

    [bug] больше переводов строк
      * благодарности клиенту

--- lang/ru/options.php
+++ lang/ru/options.php
@@ -1,5 +1,14 @@
 <?php
 // ру
-$MESS['MAX_API_LOGIN']      = 'Логин в системе';
-$MESS['MAX_API_PASSWORD']   = 'Пароль';
-$MESS['MAX_UPLOAD_PATH']    = 'Путь к файлам фото (относительно папки upload)';
+$MESS['MAX_API_LOGIN']    = 'Логин в системе';
+$MESS['MAX_API_PASSWORD'] = 'Пароль';
+$MESS['MAX_UPLOAD_PATH']  = 'Путь к файлам фото (относительно папки upload)';
+
+$MESS['MAX_PHOTO_SIZE_BIG_WIDTH']  = 'Максимальная ширина большого изображения';
+$MESS['MAX_PHOTO_SIZE_BIG_HEIGHT'] = 'Максимальная высота большого изображения';
+$MESS['MAX_PHOTO_SIZE_MED_WIDTH']  = 'Максимальная ширина среднего изображения';
+$MESS['MAX_PHOTO_SIZE_MED_HEIGHT'] = 'Максимальная высота среднего изображения';
+$MESS['MAX_PHOTO_SIZE_SML_WIDTH']  = 'Максимальная ширина изображения анонса)';
+$MESS['MAX_PHOTO_SIZE_SML_HEIGHT'] = 'Максимальная высота изображения анонса';
+$MESS['PHOTO_TAB_SET']             = 'Настройки изображений';
+$MESS['PHOTO_TAB_TITLE_SET']       = 'Настройки изображений';
